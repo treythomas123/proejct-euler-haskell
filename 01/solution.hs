62f5da285379f5764bcfa1035b3933facdb20757
treythomas123 <treythomas123@gmail.com>
2015-04-10 09:05:03 -0600

    Add solution to problem 1

---
+++ 01/solution.hs
@@ -0,0 +1,11 @@
+-- Solution to Project Euler problem 1
+-- By Trey Thomas
+--
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+-- get 3, 5, 6 and 9. The sum of these multiples is 23.
+--
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+multiples_of_3_or_5 = [x | x <- [1,2..], mod x 3 == 0 || mod x 5 == 0]
+
+main = print $ sum $ takeWhile (< 1000) multiples_of_3_or_5
