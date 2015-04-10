d06837e5f911a3ee5a064793fd98bb779da249cf
treythomas123 <treythomas123@gmail.com>
2015-04-10 09:10:46 -0600

    Change to camelCase

--- 01/solution.hs
+++ 01/solution.hs
@@ -6,6 +6,6 @@
 --
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
-multiples_of_3_or_5 = [x | x <- [1,2..], mod x 3 == 0 || mod x 5 == 0]
+multiplesOf3Or5 = [x | x <- [1,2..], mod x 3 == 0 || mod x 5 == 0]
 
-main = print $ sum $ takeWhile (< 1000) multiples_of_3_or_5
+main = print $ sum $ takeWhile (< 1000) multiplesOf3Or5
