e03ca6b16938446f27784ef0dc0303361ebe6f82
treythomas123 <treythomas123@gmail.com>
2015-05-07 18:21:06 -0700

    Add solution to problem 43

---
+++ 43/solution.hs
@@ -0,0 +1,36 @@
+-- Solution to Project Euler problem 43
+-- By Trey Thomas
+--
+-- The number, 1406357289, is a 0 to 9 pandigital number because it is made up
+-- of each of the digits 0 to 9 in some order, but it also has a rather
+-- interesting sub-string divisibility property.
+--
+-- Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note
+-- the following:
+--
+-- d2d3d4=406 is divisible by 2
+-- d3d4d5=063 is divisible by 3
+-- d4d5d6=635 is divisible by 5
+-- d5d6d7=357 is divisible by 7
+-- d6d7d8=572 is divisible by 11
+-- d7d8d9=728 is divisible by 13
+-- d8d9d10=289 is divisible by 17
+--
+-- Find the sum of all 0 to 9 pandigital numbers with this property.
+
+import Data.List
+
+isSubstringDivisible :: String -> Bool
+isSubstringDivisible n
+    | not $ subDivisibleBy 1 2 = False
+    | not $ subDivisibleBy 2 3 = False
+    | not $ subDivisibleBy 3 5 = False
+    | not $ subDivisibleBy 4 7 = False
+    | not $ subDivisibleBy 5 11 = False
+    | not $ subDivisibleBy 6 13 = False
+    | not $ subDivisibleBy 7 17 = False
+    | otherwise = True
+    where
+        subDivisibleBy d f = mod (read (take 3 $ drop d $ n) :: Int) f == 0
+
+main = print $ sum $ map (read::String->Int) $ filter isSubstringDivisible $ permutations ['0'..'9']
